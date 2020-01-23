3a2a486e23a19d335de5cda5e160d998eed758e1
luminousvilax <luminousvilax@gmail.com>
2020-01-24 02:57:31 +0800

    Core Magic Power System edited.

---
+++ src/main/java/Luminous/relics/MirrorRelic.java
@@ -0,0 +1,52 @@
+package Luminous.relics;
+
+import Luminous.powers.MagicPowerSystem;
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import Luminous.DefaultMod;
+import Luminous.util.TextureLoader;
+
+import static Luminous.DefaultMod.makeRelicOutlinePath;
+import static Luminous.DefaultMod.makeRelicPath;
+
+public class MirrorRelic extends CustomRelic {
+
+    /*
+     * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
+     *
+     * Gain 1 energy.
+     */
+
+    // ID, images, text.
+    public static final String ID = DefaultMod.makeID("MirrorRelic");
+
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("MirrorRelic.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("placeholder_relic.png"));
+
+    public MirrorRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.STARTER, LandingSound.MAGICAL);
+    }
+
+    // Flash at the start of Battle.
+    @Override
+    public void atBattleStart() {
+        flash();
+        //AbstractDungeon.actionManager.addToBottom((AbstractGameAction)new RelicAboveCreatureAction((AbstractCreature)AbstractDungeon.player, (AbstractRelic)this));
+       AbstractDungeon.actionManager.addToBottom((AbstractGameAction)new ApplyPowerAction((AbstractCreature)AbstractDungeon.player, (AbstractCreature)AbstractDungeon.player, (AbstractPower)new MagicPowerSystem((AbstractCreature)AbstractDungeon.player, 1)));
+    }
+
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
